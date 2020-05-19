0feeba165f77f5b077d0d437449c40e6ebe8d181
wanggang1906 <1294901911@qq.com>
2020-05-19 17:53:52 +0800

    面向对象

--- test06_Oop.cpp
+++ test06_Oop.cpp
@@ -1,9 +1,56 @@
-#include "test06_Oop.h"
+﻿#include "test06_Oop.h"
 #include <iostream>
+#include<string>
 
 
+
+// 面向对象
 
 void test06_Oop::classMain()
 {
 	std::cout << "test06_Oop";
 }
+
+
+
+
+// C++中类和结构体比较相似
+// 类中可以定义属性和方法，结构体中声明变量，是一组变量组成的数据结构，操作是
+// 一个空类也含有四种函数，构造函数，析构函数，赋值函数，拷贝函数
+
+
+
+// 基类Personal
+class Personal
+{
+public:
+	std::string name;
+	float age;
+	int a;
+
+	void saidWord()
+	{
+		std::cout << "输出";
+	}
+	void saidBay();
+};
+
+/*
+// :: 是域运算符，在类中声明方法，但未实现，在类外实现时用::指明是那个类的方法
+void Personal::saidBay(){
+	std::cout << "bay";
+}
+*/
+
+// 子类PerA 以public的方式继承 Personal
+class PerA : public Personal
+{
+public:
+	int perAParticularAttribute = 021;
+
+	void saidSelfName()
+	{
+		std::cout << "我是：" << perAParticularAttribute;
+	}
+
+};
--- test06_Oop.h
+++ test06_Oop.h
@@ -1,7 +1,9 @@
-#pragma once
+﻿#pragma once
+
+// 条件编译
+
 class test06_Oop
 {
 public:
 	void classMain();
 };
-
