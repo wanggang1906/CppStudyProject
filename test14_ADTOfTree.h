1710914c4331f36473f3ebc9cf8a9cddd1c423fe
wanggang1906 <1294901911@qq.com>
2020-06-11 18:06:57 +0800

    树

--- test14_ADTOfTree.h
+++ test14_ADTOfTree.h
@@ -1,6 +1,27 @@
 ﻿#pragma once
 class test14_ADTOfTree
 {
+	typedef int ElemType;
+
+	// 顺序存储
+
+	// 链式存储
+	typedef struct BinaryTreeNode {
+		ElemType data;
+		struct BinaryTreeNode* lchild, * rchild; // 左/右孩子指针
+	}BiTNode, * BiTree;
+
+
+	// 树的存储
+	// 双亲表示法
+
+	// 孩子表示法
+
+	// 孩子兄弟表示法
+
+
+
+
 public:
 	void classMain();
 };
