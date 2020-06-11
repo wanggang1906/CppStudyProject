e429f737120d5ce3f7684417c6c56509c5f20c15
wanggang1906 <1294901911@qq.com>
2020-06-11 11:17:20 +0800

    队列，链队

--- test13_ADTOfQueue.cpp
+++ test13_ADTOfQueue.cpp
@@ -6,6 +6,7 @@
 // 队列
 
 
+
 void test13_ADTOfQueue::classMain()
 {
 	std::cout << "test13_ADTOfQueue" << "\n";
@@ -29,17 +30,85 @@ void test13_ADTOfQueue::classMain()
 // 循环队列
 
 
+// 初始化队列
+void test13_ADTOfQueue::initCycleQueue(SqQueue& q)
+{
+	q.rear = q.front = 0;
+}
+
+// 判队空
+bool test13_ADTOfQueue::isEmptyOfCycleQueue(SqQueue q)
+{
+	if (q.rear == q.front)
+		return true;
+	return false;
+}
+
+// 元素入队
+bool test13_ADTOfQueue::enterElemOfCycleQueue(SqQueue& q, ElemType x)
+{
+	if ((q.rear + 1) % 10 == q.front)
+		return false;
+	q.data[q.rear] = x;
+	q.rear = (q.rear + 1) % 10;
+	return true;
+}
+
+// 元素出队
+bool test13_ADTOfQueue::deleteElemOfCycleQueue(SqQueue& q, ElemType& x)
+{
+	if (q.rear == q.front)
+		return false;
+	x = q.data[q.front];
+	q.front = (q.front + 1) % 10;
+	return true;
+}
+
+// 取对头元素
+
 
 // 链式队列
 
-
 // 初始化
+void test13_ADTOfQueue::initLinkQueue(LinkQueue& q)
+{
+	q.front = q.rear = (LinkQueueNode*)malloc(sizeof(LinkQueueNode));
+	q.front->next = NULL;
+}
 
 // 判队空
+bool test13_ADTOfQueue::isLinkQueueEmpty(LinkQueue q)
+{
+	if (q.front == q.rear)
+		return true;
+	else
+		return false;
+}
 
 // 入队
+void test13_ADTOfQueue::enterELemOfLinkQueue(LinkQueue& q, ElemType x)
+{
+	LinkQueueNode* s;
+	s = (LinkQueueNode*)malloc(sizeof(LinkQueueNode));
+	s->data = x;
+	s->next = NULL;
+	q.rear->next = s;
+	q.rear = s;
+}
 
 // 出队
+bool test13_ADTOfQueue::deleteElemOfLinkQueue(LinkQueue& q, ElemType& x)
+{
+	LinkQueueNode* p;
+	if (q.front == q.rear)
+		return false; // 空队
+	p = q.front->next;
+	x = p->data;
+	if (q.rear == p)
+		q.rear = q.front; // 若原队列只有一个节点，删除后变空队
+	free(p);
+	return true;
+}
 
 
 
--- test13_ADTOfQueue.h
+++ test13_ADTOfQueue.h
@@ -1,6 +1,40 @@
 ﻿#pragma once
 class test13_ADTOfQueue
 {
+	typedef int ElemType;
+	// 顺序队
+	typedef struct SqQueue {
+		ElemType data[10];
+		int front, rear; // 队头/队尾指针
+	};
+
+	// 循环队列
+
+
+	// 链队
+	typedef struct LinkQueueNode { // 链式队列的节点
+		ElemType data;
+		struct LinkQueueNode* next;
+	};
+	typedef struct LinkQueue { // 链式队列
+		LinkQueueNode* front, * rear; // 队头/队尾
+	};
+
 public:
 	void classMain();
+
+	// 顺序队
+
+	// 循环队列
+	void initCycleQueue(SqQueue& q);
+	bool isEmptyOfCycleQueue(SqQueue q);
+	bool enterElemOfCycleQueue(SqQueue& q, ElemType x);
+	bool deleteElemOfCycleQueue(SqQueue& q, ElemType& x);
+
+	// 链队
+	void initLinkQueue(LinkQueue& q);
+	bool isLinkQueueEmpty(LinkQueue q);
+	void enterELemOfLinkQueue(LinkQueue& q, ElemType x);
+	bool deleteElemOfLinkQueue(LinkQueue& q, ElemType& x);
+
 };
