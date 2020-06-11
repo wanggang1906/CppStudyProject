ea45bd10e93df350aae7fee0d1800a27f694a593
wanggang1906 <1294901911@qq.com>
2020-06-11 10:37:56 +0800

    栈

--- test12_ADTOfStack.cpp
+++ test12_ADTOfStack.cpp
@@ -3,24 +3,63 @@
 #include <string>
 
 // 栈
+// @1 - 首先是一种线性表
+// 
+// 
+// 
+// 
+// 
+// 
 
 void test12_ADTOfStack::classMain()
 {
 	std::cout << "test12_ADTOfStack" << "\n";
 }
 
+
 // 顺序栈
 
 // 初始化一个空栈
+void test12_ADTOfStack::initStack(SqStack& s)
+{
+	s.top = -1;
+}
 
 // 判断一个栈是否为空
-
+bool test12_ADTOfStack::isStackEmpty(SqStack s)
+{
+	if (s.top == -1)
+		return true;
+	else
+		return false;
+}
 
 // 元素进栈
+bool test12_ADTOfStack::pushStack(SqStack& s, ElemType x)
+{
+	if (s.top == 10 - 1)
+		return false;
+	s.data[++s.top] = x;
+	return true;
+}
 
 // 元素出栈
+bool test12_ADTOfStack::popStack(SqStack& s, ElemType& x)
+{
+	if (s.top == -1)
+		return false;
+	x = s.data[s.top--];
+	return true;
+}
 
 // 取栈顶元素
+bool test12_ADTOfStack::getTopElem(SqStack s, ElemType& x)
+{
+	if (s.top == -1)
+		return false;
+	x = s.data[s.top];
+	return true;
+}
 
 // 销毁栈
 
--- test12_ADTOfStack.h
+++ test12_ADTOfStack.h
@@ -1,6 +1,61 @@
 ﻿#pragma once
 class test12_ADTOfStack
 {
+	// C++成员访问限定符
+	// class中如果对成员变量和成员函数不指定访问权限限定符，默认为private
+	// 而struct则默认为public。
+#define MSize 10;
+	//int MaxSize = 10;
+	// 参考
+	// https://blog.csdn.net/lizhengze1117/article/details/98746628
+
+	// C++定义常量
+	//const int MaxSize = 10;
+	//constexpr int MaxSize = 10;
+	// const 变量的初始化可以延迟到运行时，而 constexpr 变量必须在编译时进行初始化。
+	// 所有 constexpr 变量均为常量，因此必须使用常量表达式初始化。
+	typedef int ElemType;
+	// 顺序栈
+	typedef struct SqStack {
+		ElemType data[10];
+		int top; // 栈顶指针
+	};
+
+	// 共享栈 - 共享栈的数组
+	typedef struct ShareStack {
+		ElemType data[10];
+		int top0; // 0号栈的栈顶指针 - 初值为数组头
+		int top1; // 1号栈的栈顶指针 - 初值为数组尾
+	};
+
+	// 链栈
+	typedef struct LinkStackNode {
+		ElemType data;
+		struct LinkStackNode* next; // 指针域
+	} *LiStack;
+
+
+
+
 public:
 	void classMain();
+
+	// 基本操作
+	void initStack(SqStack& S);
+	bool isStackEmpty(SqStack S);
+	bool pushStack(SqStack& S, ElemType x);
+	bool popStack(SqStack& S, ElemType& x);
+	bool getTopElem(SqStack S, ElemType& x);
+
+
+
+
+
+
+
+
+
+
+
+
 };
